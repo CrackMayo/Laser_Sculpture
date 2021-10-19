86ee5c2d283dd9140226f974a13ca23553264a25
CrackMayo <tamayo998@hotmail.com>
2021-10-18 21:20:26 -0500

    Laser v0.1 added

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int h = in.nextInt();
+        int l;
+
+        while(h != 0){
+            l= in.nextInt();
+            in.nextLine();
+            int[] vector= new int [l];
+            for (int i = 0; i < vector.length; i++) {
+                vector[i]=in.nextInt();
+            }
+            int contador=0;
+            int actual = h;
+
+            System.out.println(contador);
+            h = in.nextInt();
+        }
+    }
+
+}
